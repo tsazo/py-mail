b77334d3dd7731a3177cefa15b74605ed4bd3c4c
Trinity Sazo <tsazo@andover.edu>
2016-07-23 08:53:34 -0400

    Sending email via python

--- simple_email.py
+++ simple_email.py
@@ -7,9 +7,9 @@ from email.MIMEText import MIMEText
 # and let Python do the rest...
 fromaddr = "andoverace10@gmail.com"
 password = "gobigblue"
-toaddr = ""  # WARNING: change this
-subject = "Sample Subject"
-body = "Sample body"
+toaddr = ""
+subject = "Hi"
+body = "I'm coding."
 ##############################################
 
 print "Sending email from %s to %s..." % (fromaddr, toaddr)
